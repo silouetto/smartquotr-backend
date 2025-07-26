c359de2862b97eaf37cf6474205eaf50191af2b9
Silouetto <sillouetto@gmail.com>
2025-07-26 11:45:56 -0700

    🛡️ Fix CORS allow origin for frontend

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ app = FastAPI()
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.smartquotr.com"],
+    allow_origins=[
+        "https://www.smartquotr.com",
+        "https://www.smartquotr.com/"  
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
