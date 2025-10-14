b2abc912fcda024ff5844259bc2b548c755cb815
silouetto <sillouetto@gmail.com>
2025-10-13 18:23:42 -0700

    Update local_businesses.py
    added permanent local businesses

--- routes/data/local_businesses.py
+++ routes/data/local_businesses.py
@@ -28,5 +28,77 @@ LOCAL_BUSINESSES = [
         "location": "Portland, OR",
         "website": "https://brightcleanwindows.com",
         "timestamp": 1694841600
+    },
+    {
+        "name": "Motolux Collision Center",
+        "email": "info@motoluxbodyshop.com",
+        "category": "Auto Repair",
+        "description": "Looking for a reliable auto body shop in Vancouver WA? At Motolux Body Shop, we specialize in collision repair, dent removal, paintless dent repair, windshield replacement, bumper repair, and auto glass services. Our certified team provides OEM-quality repairs with fast turnaround and affordable pricing.",
+        "location": "Vancouver, WA",
+        "website": "https://www.motoluxbodyshop.com/",
+        "timestamp": 1694841600
+    },
+    {
+        "name": "Lightning Collision Services",
+        "email": "lightningcollisionservices@gmail.com",
+        "category": "Auto Repair",
+        "description": "Services: Insurance Company Assistance, Lifetime Warranty, Towing, Automotive Glass Replacement, Motorcycle Repair, Detailing, Paintless Dent Repair",
+        "location": "Vancouver, WA",
+        "website": "",
+        "timestamp": 1694841600
+    },
+    {
+        "name": "Ultimate Auto Glass & Electronics",
+        "email": "e.king@utimateautoglass.com",
+        "category": "Other",
+        "description": "Ultimate Auto Glass & Electronics is your #1 choice for high-quality Auto Glass Repair, Replacement, Calibrations, and Automotive OEM Electronics services.",
+        "location": "Portland, OR",
+        "website": "",
+        "timestamp": 1694841600
+    },
+    {
+        "name": "NG USED CARS LC",
+        "email": "Ngusedcars4@gmail.com",
+        "category": "Dealership",
+        "description": "At NG USED CARS LLC, our mission is to provide high-quality used cars at affordable prices. We believe in transparency and customer satisfaction, ensuring you find the perfect vehicle for your needs.",
+        "location": "Portland, OR",
+        "website": "https://ngusedcars.com/",
+        "timestamp": 1694841600
+    },
+    {
+        "name": "NG METALS LLC",
+        "email": "ngmetals2020@gmail.com",
+        "category": "Other",
+        "description": "Scrap metal buyer",
+        "location": "Portland, OR",
+        "website": "",
+        "timestamp": 1694841600
+    },
+    {
+        "name": "Columbia Sales & Service",
+        "email": "contact@columbiasalesandservice.com",
+        "category": "Dealership",
+        "description": "Everything we do is designed to make your car buying experience as easy and enjoyable as possible. Competitive pricing, knowledgeable staff and superior customer service all add up to create a simple and painless way to purchase a world class automobile at a great price.",
+        "location": "Portland, OR",
+        "website": "https://www.columbiasalesandservice.com/",
+        "timestamp": 1694841600
+    },
+    {
+        "name": "SAMEDAY Auto Scratch and Dent Repair",
+        "email": "elena.c@samedaynw.com",
+        "category": "Auto Repair",
+        "description": "We specialize in providing efficient auto repair services without compromising quality. Our expert team is dedicated to swiftly restoring your vehicle to its prime condition. ",
+        "location": "Beaverton, OR",
+        "website": "https://samedaynw.com/",
+        "timestamp": 1694841600
+    },
+    {
+        "name": "Granados",
+        "email": "",
+        "category": "Dealership",
+        "description": "Auto Whosale and automtive service repairs by appointment only, (971)-227-7039",
+        "location": "Milwaukie, OR",
+        "website": "https://brightcleanwindows.com",
+        "timestamp": 1694841600
     }
 ]
